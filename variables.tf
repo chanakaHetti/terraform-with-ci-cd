474ec27fae1f52c8ae5c0dd9ab66fbb9145a02a3
chanakaHetti <cdileep40@gmail.com>
2023-11-26 10:31:15 +0000

    Adding the route table

--- variables.tf
+++ variables.tf
@@ -5,3 +5,47 @@ variable "vpc_cidr" {
 variable "all_cidr" {
     type = string
 }
+
+variable "public_subnet1_cidr" {
+    type = string
+}
+
+variable "public_subnet2_cidr" {
+    type = string
+}
+
+variable "private_subnet_cidr" {
+    type = string
+}
+
+variable "availability_zone" {
+    type = string
+}
+
+variable "jenkins_port" {
+    type = number
+}
+
+variable "sonarqube_port" {
+    type = number
+}
+
+variable "grafana_port" {
+    type = number
+}
+
+variable "http_port" {
+    type = number
+}
+
+variable "ssh_port" {
+    type = number
+}
+
+variable "key_name" {
+    type = string
+}
+
+variable "key_value" {
+    type = string
+}
